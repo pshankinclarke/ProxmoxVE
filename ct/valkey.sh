69bfbe2709ecc76de3ced6046f03f4b77a394d50
pshankinclarke <parkerwsc1@gmail.com>
2026-04-30 15:53:18 -0700

    remove gateway icon from credentials path

--- ct/valkey.sh
+++ ct/valkey.sh
@@ -42,4 +42,4 @@ description
 msg_ok "Completed successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
 echo -e "${INFO}${YW} Connection details saved inside the container:${CL}"
-echo -e "${TAB}${GATEWAY}${BGN}/root/valkey.creds${CL}"
+echo -e "${TAB}${BGN}/root/valkey.creds${CL}"
